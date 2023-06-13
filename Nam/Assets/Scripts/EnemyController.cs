d6ecb00e2b28afc9d4fac01db588ca888d5fe65d
yknam4222 <yyhnam2222@gmail.com>
2023-06-13 13:49:12 +0900

    Nam A0613_2

--- Nam/Assets/Scripts/EnemyController.cs
+++ Nam/Assets/Scripts/EnemyController.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class EnemyController : MonoBehaviour
 {
-
-    public Node Target;
+    public Node Target = null;
+    public List<Vector3> vertices = new List<Vector3>();
 
     private float Speed;
+
+    public Material material;
 
     Vector3 LeftCheck;
     Vector3 RightCheck;
@@ -28,7 +30,7 @@ public class EnemyController : MonoBehaviour
         Rigidbody rigid = GetComponent<Rigidbody>();
         rigid.useGravity = false;
 
-        Target = GameObject.Find("ParentObject").transform.GetChild(0).GetComponent<Node>();
+        //Target = GameObject.Find("ParentObject").transform.GetChild(0).GetComponent<Node>();
     }
     private void Start()
     {
@@ -43,12 +45,44 @@ public class EnemyController : MonoBehaviour
         Angle = 45.0f;
 
         move = false;
-
     }
 
     private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+            {
+                MeshFilter meshFilter = hit.transform.gameObject.GetComponent<MeshFilter>();
+
+                Vector3[] verticesPoint = meshFilter.mesh.vertices;
+
+                for(int i = 0;  i < verticesPoint.Length; ++i)
+                {
+                    if (!vertices.Contains(verticesPoint[i]) 
+                        && verticesPoint[i].y < transform.position.y + 0.05f
+                        && transform.position.y < verticesPoint[i].y + 0.05f)
+                    {
+                        vertices.Add(verticesPoint[i]);
+                    }
+                }
+
+            }
+
+            for(int i = 0; i < vertices.Count; ++i)
+            {
+                GameObject obj = new GameObject(i.ToString());
+                obj.transform.position = new Vector3(
+                            hit.transform.position.x + vertices[i].x * hit.transform.localScale.x,
+                            transform.position.y,
+                            hit.transform.position.z + vertices[i].z * hit.transform.localScale.z);
+
+                obj.AddComponent<myGizmo>();
+            }
+        }
+
         if (Target)
         {
             Vector3 Direction = (Target.transform.position - transform.position).normalized;
@@ -106,36 +140,39 @@ public class EnemyController : MonoBehaviour
 
     }
 
+    //void function()
+    //{
+    //    if (move)
+    //        return;
 
-    void function()
+    //    move = true;
+    //    StartCoroutine(SetMove());
+    //}
+
+    //IEnumerator SetMove()
+    //{
+    //    float time = 0.0f;
+
+
+    //    while (time < 1.0f)
+    //    {
+    //        time += Time.deltaTime;
+
+    //        yield return null;
+    //    }
+
+    //    move = false;
+    //}
+
+    private void LateUpdate()
     {
-        if (move)
-            return;
-
-        move = true;
-        StartCoroutine(SetMove());
-    }
-
-    IEnumerator SetMove()
-    {
-        float time = 0.0f;
-
-
-        while (time < 1.0f)
-        {
-            time += Time.deltaTime;
-
-            yield return null;
-        }
-
-        move = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         move = false;
 
-        if (Target.transform.name == other.transform.name)
-            Target = Target.Next;
+        //if (Target.transform.name == other.transform.name)
+        //    Target = Target.Next;
     }
 }
