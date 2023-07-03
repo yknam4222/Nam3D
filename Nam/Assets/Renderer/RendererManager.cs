0708f0760f21cbd2f31f79359059a95631e2e34b
yknam4222 <yyhnam2222@gmail.com>
2023-07-03 11:02:59 +0900

    12

--- Nam/Assets/Renderer/RendererManager.cs
+++ Nam/Assets/Renderer/RendererManager.cs
@@ -6,16 +6,38 @@ public class RendererManager : MonoBehaviour
 {
     private Renderer renderer;
 
+    private const string path = "Legacy Shaders/Transparent/Specular";
+
     void Start()
     {
         renderer = GetComponent<Renderer>();
-        Material material = renderer.material;
-
-        material.SetFloat("_Alpha", 0.5f);
     }
 
-    void Update()
+    IEnumerator SetColor(Renderer renderer)
     {
-        
+        Material material = new Material(Shader.Find(path));
+
+        while (true)
+        {
+            yield return null;
+
+            renderer.material = material;
+
+            Color color = renderer.material.color;
+
+            color.a = 0.5f;
+            renderer.material.color = color;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (renderer != null)
+            {
+                StartCoroutine(SetColor(renderer));
+            }
+        }
     }
 }
