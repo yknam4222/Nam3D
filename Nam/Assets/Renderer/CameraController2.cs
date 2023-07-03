15f5ae6266d2a4ff843c01b25255ec2630d6d7ae
yknam4222 <yyhnam2222@gmail.com>
2023-07-03 13:49:23 +0900

    1234

--- Nam/Assets/Renderer/CameraController2.cs
+++ Nam/Assets/Renderer/CameraController2.cs
@@ -9,6 +9,12 @@ public class CameraController2 : MonoBehaviour
     private Transform target;
     private float distance;
 
+    public LayerMask mask;
+
+    private bool Check;
+
+    private const string path = "Legacy Shaders/Transparent/Specular";
+
     private void Awake()
     {
         camera = Camera.main;
@@ -21,6 +27,8 @@ public class CameraController2 : MonoBehaviour
         direction = (target.position - transform.position).normalized;
 
         distance = Vector3.Distance(target.position, transform.position);
+
+        Check = false;
     }
 
     void Update()
@@ -30,16 +38,54 @@ public class CameraController2 : MonoBehaviour
 
         RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit, distance))
+        if (Physics.Raycast(ray, out hit, distance, mask))
         {
-            if(hit.transform != null)
+            if (hit.transform != null)
             {
-                if(hit.transform.name != "Cube")
-                {
-
-                }
                 Debug.Log(hit.transform.name);
+                if (Check == false)
+                {
+                    Check = true;
+                    Renderer renderer = hit.transform.GetComponent<Renderer>();
+
+                    if (renderer != null)
+                        StartCoroutine(SetColor(renderer));
+                }
             }
+            else if(hit.transform == null)
+            {
+                Check = false;
+
+                Debug.Log("zzzzzzzzz");
+            }
+            /*
+            else
+            {
+                Check = false;
+                renderer = null;
+            }
+             */
+        }
+
+
+    }
+
+    IEnumerator SetColor(Renderer renderer)
+    {
+        // Color값 변경이 가능한 Shader로 변경
+        Material material = new Material(Shader.Find(path));
+
+        //변경된 Shader의 color값들 받아옴.
+        Color color = renderer.material.color;
+
+        //color.a 값이 0.5f 보다 큰 경우에만 반복.
+        while (0.5f < color.a)
+        {
+            yield return null;
+
+            // Alpha = 1 : Alpha -= Time.deltaTime
+            color.a -= Time.deltaTime;
+            renderer.material.color = color;
         }
     }
 }
