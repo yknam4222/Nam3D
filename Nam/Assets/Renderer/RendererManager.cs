f5dc2db4c7f4f345cd7fa74f8ffaf95cbf4c6920
yknam4222 <yyhnam2222@gmail.com>
2023-07-03 11:13:10 +0900

    123

--- Nam/Assets/Renderer/RendererManager.cs
+++ Nam/Assets/Renderer/RendererManager.cs
@@ -15,17 +15,19 @@ public class RendererManager : MonoBehaviour
 
     IEnumerator SetColor(Renderer renderer)
     {
+        // Color값 변경이 가능한 Shader로 변경
         Material material = new Material(Shader.Find(path));
 
-        while (true)
+        //변경된 Shader의 color값들 받아옴.
+        Color color = renderer.material.color;
+
+        //color.a 값이 0.5f 보다 큰 경우에만 반복.
+        while (0.5f < color.a)
         {
             yield return null;
 
-            renderer.material = material;
-
-            Color color = renderer.material.color;
-
-            color.a = 0.5f;
+            // Alpha = 1 : Alpha -= Time.deltaTime
+            color.a -= Time.deltaTime;
             renderer.material.color = color;
         }
     }
